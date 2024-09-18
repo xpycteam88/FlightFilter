ca626d20ea511003be9f7d988beff6095e950f17
xpycteam88 <sicuno@mail.ru>
2024-09-18 22:47:49 +0300

    Init commit

---
+++ src/main/java/com/gridnine/FlightFilterApplication.java
@@ -0,0 +1,13 @@
+package com.gridnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFilterApplication.class, args);
+	}
+
+}
